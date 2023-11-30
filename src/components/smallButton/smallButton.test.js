2ce96af26fc20a4639c4997dba1ca1058c48c68c
chunyang <chunyang@sph.com.sg>
2023-11-30 14:12:55 +0800

    WIP

---
+++ src/components/smallButton/smallButton.test.js
@@ -0,0 +1,7 @@
+import SmallButton from "./smallButton"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+
+describe("Small Button", () => {
+	test("Digits render on calculator screen", () => {})
+})
